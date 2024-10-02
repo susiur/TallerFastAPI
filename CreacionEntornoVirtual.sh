6edf47fe5be5786b6a7b1c452311b79a3196cf0c
VectorRes <vhrestrepomedina@gmail.com>
2024-10-02 09:33:48 -0500

    Creacion del Entorno Virtual

---
+++ CreacionEntornoVirtual.sh
@@ -0,0 +1,23 @@
+#download postgreSQL
+sudo apt update
+sudo apt install postgresql-client
+
+#creation for the virtual enviroment
+mkdir proyecto
+cd proyecto
+touch requierements.txt
+mkdir EntornoVirtual
+cd EntornoVirtual
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+echo "export PATH="~/miniconda3/bin:$PATH"" >> ~/.bashrc
+conda create --name myenv python=3.9
+conda activate myenv
+cd ..
+echo "fastapi" > requierements.txt
+echo "uvicorn" >> requierements.txt
+echo "sqlalchemy" >> requierements.txt
+echo "asyncpg" >> requierements.txt
+echo "psycopg2-binary" >> requierements.txt
+echo "alembic" >> requierements.txt
+pip install -r requierements.txt
